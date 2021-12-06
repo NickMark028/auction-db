7f2e9db9ee890e43a7b72a04c4a6010e3904c781
Nick Mark <nickmark028@gmail.com>
2021-12-06 23:31:11 +0700

    Add create views scripts

---
+++ 3 Create views.sql
@@ -0,0 +1,25 @@
+USE `Auction`;
+
+DROP VIEW IF EXISTS `AdminView`;
+CREATE VIEW `AdminView`
+AS
+	SELECT	U.*
+	FROM	`Admin` A JOIN `User` U ON A.`id` = U.`id`;
+
+DROP VIEW IF EXISTS `BidderView`;
+CREATE VIEW `BidderView`
+AS
+	SELECT	U.*, B.`address`, B.`verifed`, B.`score`, B.`isDeleted`
+	FROM	`Bidder` B JOIN `User` U ON B.`id` = U.`id`;
+
+DROP VIEW IF EXISTS `SellerView`;
+CREATE VIEW `SellerView`
+AS
+	SELECT	BV.*, S.`expiredTime`, S.`active`
+	FROM	`Seller` S JOIN `BidderView` BV ON S.`id` = BV.`id`;
+
+DROP VIEW IF EXISTS `ProductView`;
+CREATE VIEW `ProductView`
+AS
+	SELECT	P.*, BP.`topBidderId`, BP.`bidderOwnerId`, BP.`currentPrice`, BP.`auctionLogCount`, BP.`bidderCount`, BP.`remainingTime`
+	FROM	`Product` P JOIN `BiddedProduct` BP ON P.`id` = BP.`id`;
