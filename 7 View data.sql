818849899021cdfa9003e231ef94544225ac3c36
Nick Mark <nickmark028@gmail.com>
2021-12-06 23:31:28 +0700

    Add View data script

---
+++ 7 View data.sql
@@ -0,0 +1,16 @@
+USE `Auction`;
+
+
+-- View data
+-- SHOW TABLES;
+
+SELECT * FROM `ChangeRoleStatus`;
+SELECT * FROM `BiddedProductStatus`;
+SELECT * FROM `Category`;
+
+SELECT * FROM `AdminView`;
+SELECT * FROM `BidderView`;
+SELECT * FROM `ChangeRoleLog`;
+SELECT * FROM `SellerView`;
+
+SELECT * FROM `ProductView`;
